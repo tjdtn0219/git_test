4061c9ac3774cb7bfe40d6261776c4238607a352
장성수 <tjdtn0219@ajou.ac.kr>
2021-07-19 01:54:57 +0900

    SignUpActivity Toast

--- app/src/main/java/com/example/sns_project_sample/SignUpActivity.java
+++ app/src/main/java/com/example/sns_project_sample/SignUpActivity.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
@@ -44,6 +45,7 @@ public class SignUpActivity extends AppCompatActivity {
             switch(v.getId()) {
                 case R.id.SignUpButton:
                     Log.e("클릭","클릭");
+                    signUp();
                     break;
             }
         }
@@ -52,23 +54,37 @@ public class SignUpActivity extends AppCompatActivity {
     private void signUp(){
         String email = ((EditText)findViewById(R.id.emailEditText)).getText().toString();
         String password = ((EditText)findViewById(R.id.passwordEditText)).getText().toString();
+        String passwordCheck = ((EditText)findViewById(R.id.passwordcheckEditText)).getText().toString();
 
-        mAuth.createUserWithEmailAndPassword(email, password)
-                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
-                    @Override
-                    public void onComplete(@NonNull Task<AuthResult> task) {
-                        if (task.isSuccessful()) {
-                            // Sign in success, update UI with the signed-in user's information
-                            Log.d(TAG, "createUserWithEmail:success");
-                            FirebaseUser user = mAuth.getCurrentUser();
-                            //UI
-                        } else {
-                            // If sign in fails, display a message to the user.
-                            Log.w(TAG, "createUserWithEmail:failure", task.getException());
-                            //UI
-                        }
-                    }
-                });
+        if(email.length() > 0 && password.length() > 0 && passwordCheck.length() > 0) {
+            if(password.equals(passwordCheck)){
+                mAuth.createUserWithEmailAndPassword(email, password)
+                        .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                            @Override
+                            public void onComplete(@NonNull Task<AuthResult> task) {
+                                if (task.isSuccessful()) {
+                                    FirebaseUser user = mAuth.getCurrentUser();
+                                    startToast("회원가입에 성공하였습니다.");
+                                } else {
+                                    if(task.getException() != null){
+                                        startToast(task.getException().toString());
+                                    }
+                                }
+                            }
+                        });
+            }//if_password_equal
+            else{
+                startToast("비밀번호가 일치하지 않습니다.");
+            }//else_password_equal
+        }//if_length
+        else {
+            startToast("이메일 또는 비밀번호를 입력해 주세요.");
+        }//else_length
+
+    }
+
+    private void startToast(String msg){
+        Toast.makeText(this, msg, Toast.LENGTH_SHORT).show();
 
     }
 }
